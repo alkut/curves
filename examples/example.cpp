24f3662a1f2235bb830791398a18ef2327077cf2
kutasov alexsey <alexseykutasov@gmail.com>
2023-08-23 23:40:23 -0700

    small fix

--- examples/example.cpp
+++ examples/example.cpp
@@ -13,22 +13,14 @@
 #include <numbers>
 #include <cassert>
 #include <stdexcept>
+#include <ranges>
 
-bool isActuallyCircle(const std::shared_ptr<curves::ICurve>& ptr) {
+inline bool isActuallyCircle(const std::shared_ptr<curves::ICurve>& ptr) {
   return dynamic_cast<curves::Circle*>(ptr.get()) != nullptr;
 }
 
-void testSumOpenMP(
-    const std::vector<std::shared_ptr<curves::ICurve>>& container) {
-  constexpr std::size_t iterations = 10'000'000;
-  for (size_t i = 0; i < iterations; ++i) {
-    bool ok = sumNaive(container) == sumOpenMP(container);
-    // for release config
-    if (!ok) {
-      throw std::runtime_error("openMP doesn't work!!!");
-    }
-    assert(ok);
-  }
+inline float getRadius(const std::shared_ptr<curves::ICurve>& ptr) {
+  return dynamic_cast<curves::Circle*>(ptr.get())->GetRadius();
 }
 
 int main() {
@@ -44,26 +36,21 @@ int main() {
               << curve->Derivative(time) << "\n\n";
   }
   std::vector<std::shared_ptr<curves::ICurve>> container2;
-  for (const auto& curve : container1) {
-    if (isActuallyCircle(curve)) {
-      container2.push_back(curve);
-    }
+  for (const auto& curve :
+       container1 | std::ranges::views::filter(isActuallyCircle)) {
+    container2.push_back(curve);
   }
 
   std::sort(container2.begin(), container2.end(),
             [](const auto& lhs, const auto& rhs) -> bool {
-              return dynamic_cast<curves::Circle*>(lhs.get())->GetRadius() <
-                     dynamic_cast<curves::Circle*>(rhs.get())->GetRadius();
+              return getRadius(lhs) < getRadius(rhs);
             });
 
   std::cout << "\n\nRadii of circles:\n";
   for (const auto& circle : container2) {
-    std::cout << dynamic_cast<curves::Circle*>(circle.get())->GetRadius()
-              << " ";
+    std::cout << getRadius(circle) << " ";
   }
   std::cout << "\n";
-
-  testSumOpenMP(container2);
 
   std::cout << "\nTotal sum of radii: " << sumOpenMP(container2) << "\n";
 
--- examples/summator/threads/threads_summator.cpp
+++ examples/summator/threads/threads_summator.cpp
@@ -3,15 +3,26 @@
 #include <thread>
 #include <span>
 #include <numeric>
+#include <utility>
 
 float sumThreads(
     const std::vector<std::shared_ptr<curves::ICurve>>& container) {
   const auto threadCount = std::thread::hardware_concurrency();
   std::vector<float> sumsOfRadii(threadCount, 0.f);
-  std::vector<std::span<std::shared_ptr<curves::ICurve>>> spans(threadCount);
+  std::vector<std::span<std::shared_ptr<curves::ICurve>, 0>> spans(threadCount);
 
+  const std::size_t partition_size = container.size() / threadCount;
   for (std::size_t i = 0; i < threadCount; ++i) {
     sumsOfRadii[i] = 0.f;
+    /*
+    spans[i] = std::span<std::shared_ptr<curves::ICurve>, 0>
+        (container.cbegin() + 2, container.cbegin() + 3);
+
+    spans[i] = std::move((std::span {container}).subspan(i * partition_size,
+    std::max(partition_size, container.size() - i * partition_size))); spans[i]
+    = {container.begin() + i * partition_size, container.begin() + std::max((i +
+    1) * partition_size, container.size())};
+                      */
   }
   return std::accumulate(sumsOfRadii.cbegin(), sumsOfRadii.cend(), 0.f);
 }
